3ee0360c84aeb2d10f2512350dea935004b20ab4
sunlight <kohsuke93@outlook.jp>
2018-06-14 01:28:28 +0000

    変更

--- database/migrations/2018_06_05_122727_create__users_table.php
+++ database/migrations/2018_06_05_122727_create__users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUser2Table extends Migration
 {
     /**
      * Run the migrations.
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('email');->unique();
             $table->string('name');
-            $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
